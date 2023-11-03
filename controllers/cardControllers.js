daf44905224bd94b13c718faabd3de7ba330bcc7
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-11-03 17:20:10 +0100

    created backend for pokemon

---
+++ controllers/cardControllers.js
@@ -0,0 +1,88 @@
+// const dbPool = require("../db/pgClient"); if we r connected to a database
+let jsonData = require("../data.json");
+/**
+ * Controller to serve all ducks in the database.
+ * @route `/pokemon`
+ * @param {*} req - Express request object
+ * @param {*} res - Express response object
+ * @response Array of pokemons
+ */
+
+const dataWithSprites = jsonData.map((pokemon) => ({
+  ...pokemon,
+  sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`,
+}));
+
+const getAllPokemons = (req, res) => {
+  res.json(dataWithSprites);
+};
+
+/**
+ * Controller to serve a single pokemon card based on card id.
+ * @route /pokemon/:id
+ * @param {*} req - Express request object
+ * @param {*} res - Express response object
+ * @response Single pokemon object
+ */
+
+//   req=request
+const getSinglePokemon = (req, res) => {
+  try {
+    const { pokeName } = req.params;
+    //    single object in array=pokemon= all details-name, type, base
+    const findPokemon = dataWithSprites.find(
+      (pokemon) => pokeName.toLowerCase() === pokemon.name.english.toLowerCase()
+    );
+    //   pokeName is in URL= charmander
+    // whatever user types, just make it lowercase //
+    if (!findPokemon) throw new Error("Pokemon not found.");
+
+    return res.json(findPokemon);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+// for getting enemy pokemon-easier to use id
+const getPokemonById = (req, res) => {
+  try {
+    const { id } = req.params;
+
+    //id from params is string, so can't use strict equality without converting it, should probably be done at some point, but this works for now
+    const findPokemon = dataWithSprites.find((pokemon) => id == pokemon.id);
+
+    if (!findPokemon) throw new Error("Pokemon not found.");
+
+    return res.json(findPokemon);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getPokeInfo = (req, res) => {
+  try {
+    const { pokeName, info } = req.params;
+    const findPokemon = dataWithSprites.find(
+      (pokemon) => pokeName.toLowerCase() === pokemon.name.english.toLowerCase()
+    );
+
+    if (!findPokemon) throw new Error("Pokemon not found.");
+
+    if (findPokemon.hasOwnProperty(info)) {
+      return res.json(findPokemon[info]);
+    } else {
+      return res.status(404).json({ error: "Info not found" });
+    }
+
+    //   info= type or base.. info is accessed via bracket notation, placeholder for all keys not a specific key
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getAllPokemons,
+  getSinglePokemon,
+  getPokemonById,
+  getPokeInfo,
+};
---
+++ index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config(); u can write it here or in package.json (scripts),
+//  to add env file functionality. normally vite does it automatically in frontend
+const express = require("express");
+const app = express();
+// const dbPool = require("./db/pgClient");
+const {
+  getAllPokemons,
+  getSinglePokemon,
+  getPokemonById,
+  getPokeInfo,
+} = require("./controllers/cardControllers");
+const cors = require("cors");
+
+const port = process.env.PORT || 7000;
+app.use(cors());
+
+// console.log(process.env);
+// to see in terminal all the credentials added in env file
+
+// let jsonData = require(‘./data.json’);
+app.use(express.json());
+// We enable data to be interpreted as JSON
+// And we also enable the body to be interpreted as urlencoded content
+// This is important for step 6(singleCard) and the view engine, since html forms send data as x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>
+  res.send(
+    '<p>Welcome to the PokeFight API! Go to <a href="/pokemon">/pokemon</a> to see cool things happening</p>'
+  )
+);
+
+app.route("/pokemon").get(getAllPokemons);
+app.route("/pokemon/:pokeName").get(getSinglePokemon);
+app.route("/pokemon/withid/:id").get(getPokemonById);
+app.route("/pokemon/:pokeName/:info").get(getPokeInfo);
+
+app.listen(port, () => console.log(`Welcome ${port}`));
